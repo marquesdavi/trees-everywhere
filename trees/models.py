3d3373e6fb4a93c8e4886f943d52f3cc4bd145e5
Daví Marques <50294449+marquesdavi@users.noreply.github.com>
2024-06-26 13:06:09 -0300

    implements base models

--- trees/models.py
+++ trees/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from accounts.models import Account
 
-# Create your models here.
+
+class Tree(models.Model):
+    name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class PlantedTree(models.Model):
+    age = models.IntegerField()
+    planted_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tree = models.ForeignKey(Tree, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return f"{self.tree.name} planted by {self.user.username}"
